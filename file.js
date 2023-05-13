3f058d7e43eb13c5add37530e058f34dfe4e2df8
Promise Nwankwo <promisenwankwo125@gmail>
2023-05-13 03:07:14 +0100

    edited my Api key

--- file.js
+++ file.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = `203fa770242fcd2b9555d832a88ea567`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
